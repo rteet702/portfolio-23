6b56ab65a2919a0f3844153830595f2914941ce7
Robert Teets <teetsrobert@gmail.com>
2023-01-06 21:33:21 -0700

    feat: created basic placeholder navbar component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,7 +9,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head />
-            <body>{children}</body>
+            <body>
+                <Navbar />
+                {children}
+            </body>
         </html>
     );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { NextComponentType } from "next";
+import Link from "next/link";
+import { AiFillHome } from "react-icons/ai";
+import { Source_Code_Pro } from "@next/font/google";
+
+const scp = Source_Code_Pro({ subsets: ["latin"] });
+
+const Navbar: NextComponentType = () => {
+    return (
+        <div
+            className={`p-5 bg-neutral-800 shadow-lg text-neutral-300 font-bold ${scp.className}`}
+        >
+            <nav className="flex items-center gap-5">
+                <Link href="/">
+                    <AiFillHome />
+                </Link>
+                <Link href="/about">About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/contact">Contact Me</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
